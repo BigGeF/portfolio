21020d0935770e9f6c537a060d0dd0a8d1524863
BigGeF <52731933+BigGeF@users.noreply.github.com>
2020-11-02 20:36:22 -0800

    firstCommit

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,15 +1,59 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 namespace TestProject.Controllers{
     public class HelloController:Controller{
-        [HttpGet("any")]
+        [HttpGet("")]
         public IActionResult HiThere(){
             return View("index");
         }
 
-        [HttpGet("{name}")]
-        public string Hello(string name){
-            return $"hello {name}";
+        [HttpGet("home")]
+        public IActionResult Home(){
+            return RedirectToAction("HiThere");
         }
+
+        [HttpGet("project")]
+        public IActionResult iProject(){
+
+            return View("Project");
+        }
+
+        [HttpGet("allProjects")]
+        public IActionResult allProjects(){
+            return RedirectToAction("iProject");
+        }
+
+//--------------------------------------------------------
+        [HttpGet("contact")]
+        public IActionResult icontact(){
+            return View("Contact");
+        }
+
+        [HttpGet("toContact")]
+        public IActionResult toContact(){
+            
+            return RedirectToAction("icontact");
+        }
+        [HttpPost("method")]
+        public IActionResult postMethod(string name, string number)
+        {
+            ViewBag.Name=name;
+            ViewBag.Num=number;
+            return View("time");
+        }
+// -----------------------------------------------------------------------
+        [HttpGet("time")]
+        public IActionResult Time(){
+            ViewBag.Example = "helloworld";
+            var now = DateTime.Now.ToString("MM dd, yyy h：mm tt");
+            ViewBag.clock = now;
+            return View("time",now);
+        }
+
+       
+
+
+
 
         [HttpGet("USA/{name}")]
         public string Country(string name){
